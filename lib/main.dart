8456269d2c05fc1132bf9602f29a37d8b24bc818
AntonioMartingo <106592165+AntonioMartingo@users.noreply.github.com>
2024-03-30 13:37:23 +0000

    finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,105 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: AssetImage('images/logo.jpg'),
+                ),
+                Text(
+                  'Ajsm1259',
+                  style: TextStyle(
+                    fontFamily: 'Roboto',
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  'FLUTTER DEVELOPER',
+                  style: TextStyle(
+                    fontFamily: 'Roboto',
+                    fontSize: 20.0,
+                    color: Colors.teal.shade100,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                  width: 150.0,
+                  child: Divider(
+                    color: Colors.teal.shade100,
+                  ),
+                ),
+                Container(
+                  width: 300.0, // Largura máxima do Container
+                  child: Card(
+                    margin:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    color: Colors.white,
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.phone,
+                        color: Colors.teal,
+                      ),
+                      title: Center(
+                        // Centralizando o texto horizontalmente
+                        child: Text(
+                          '+ 913 885 209',
+                          style: TextStyle(
+                            color: Colors.teal.shade900,
+                            fontFamily: 'Roboto',
+                            fontSize: 20.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 300.0, // Largura máxima do Container
+                  child: Card(
+                    margin:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    color: Colors.white,
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.email,
+                        color: Colors.teal,
+                      ),
+                      title: Center(
+                        // Centralizando o texto horizontalmente
+                        child: Text(
+                          'email@email.com',
+                          style: TextStyle(
+                            color: Colors.teal.shade900,
+                            fontFamily: 'Roboto',
+                            fontSize: 20.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
